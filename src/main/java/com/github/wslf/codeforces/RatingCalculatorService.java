47909f4076ff433419d67859992df55d536bc213
Vasyl Franchuk <WasylF@gmail.com>
2019-08-22 23:20:25 +0100

    tmp

---
+++ src/main/java/com/github/wslf/codeforces/RatingCalculatorService.java
@@ -0,0 +1,7 @@
+package com.github.wslf.codeforces;
+
+import com.github.wslf.codeforces.rating.RatingCalculator;
+
+public class RatingCalculatorService {
+  RatingCalculator ratingCalculator = new RatingCalculator(null);
+}
