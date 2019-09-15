0265654dc14ac08802b63cf31fcc78401e015fa1
Vasyl Franchuk <WasylF@gmail.com>
2019-09-15 13:16:57 +0100

    Create entities to store data

---
+++ src/main/java/com/github/wslf/codeforces/storage/entities/RatingPrediction.java
@@ -0,0 +1,10 @@
+package com.github.wslf.codeforces.storage.entities;
+
+import com.github.wslf.codeforces.rating.calculator.entities.ContestantResult;
+
+import java.util.List;
+
+public class RatingPrediction {
+  long predictionTime;
+  List<ContestantResult> prediction;
+}
