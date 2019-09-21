baae4776946bb2ad4f04a22a76530396e5e7b940
Vasyl Franchuk <WasylF@gmail.com>
2019-09-21 12:16:19 +0100

    add no args constructor

--- src/main/java/com/github/wslf/codeforces/predictor/backend/storage/github/Identifier.java
+++ src/main/java/com/github/wslf/codeforces/predictor/backend/storage/github/Identifier.java
@@ -2,11 +2,13 @@ package com.github.wslf.codeforces.predictor.backend.storage.github;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.lang.reflect.Type;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Identifier {
   String repository;
   String branch;
--- src/main/java/com/github/wslf/codeforces/predictor/backend/storage/github/OptionalParameters.java
+++ src/main/java/com/github/wslf/codeforces/predictor/backend/storage/github/OptionalParameters.java
@@ -2,9 +2,11 @@ package com.github.wslf.codeforces.predictor.backend.storage.github;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class OptionalParameters {
   String commitMessage;
 }
